bad0d877929d04f6ef52c0aacf8c4732aaa941c1
Bhavya-Makadia <bhavyamakadia11@gmail.com>
2025-10-29 19:14:27 +0530

    Basic UI and routing

---
+++ Client/watch-wise-client/src/App.jsx
@@ -0,0 +1,25 @@
+import { useState } from 'react'
+import './App.css'
+import Home from './components/home/Home'
+import Header from './components/header/Header'
+import Register from './components/register/Register'
+import Login from './components/login/Login'
+import { Route, Routes, useNavigate } from 'react-router-dom'
+
+function App() {
+  
+
+  return (
+    <>
+      <Header/>
+      <Routes>
+        <Route path="/" element={<Home/>}/>
+        <Route path="/register" element={<Register />}/>
+        <Route path="/login" element={<Login />}/>
+      </Routes>
+      
+    </>
+  )
+}
+
+export default App
